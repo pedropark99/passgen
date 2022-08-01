38554966ab7636ec37d33b64c461428aece3deec
pedropark99 <pedropark99@gmail.com>
2022-07-31 22:12:18 -0300

    Basic help printing is working

--- include/command-line-options.hpp
+++ include/command-line-options.hpp
@@ -4,19 +4,12 @@
 
 
 void printHelp();
+void formatOption (std::string shortName, std::string longName, std::string description);
 
-namespace password {
-
-    struct commandLineArgument {
-        std::string optionName;
-        std::string description;
-    };
-
-    commandLineArgument COMMAND_LINE_ARGUMENTS[2] = {
-        {"l", "The length (how many characters) of the password;"},
-        {"c", "Do you want special characters? (e.g. %$#@&*!...). Accepted values are 1 (FALSE) and 0 (NO);"}
-    };
-
+struct commandLineArgument {
+    std::string shortName;
+    std::string longName;
+    std::string description;
 };
 
 
--- src/help.cpp
+++ src/help.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <string>
 
+#include "command-line-options.hpp"
 
-void printHelp() {
+commandLineArgument COMMAND_LINE_ARGUMENTS[2] = {
+    {"-l", "--length", "The length (how many characters) of the password;"},
+    {"-s", "--special", "Do you want special characters? (e.g. %$#@&*!...). Accepted values are 1 (FALSE) and 0 (NO);"}
+};
+
+
+void printHelp () {
+    std::string opShortName;
+    std::string opLongName;
+    std::string opDescription;
+
+    std::cout << "Options for password command line:" << std::endl;
     for (int i = 0; i < 2; i++) {
-        std::cout << "help" << std::endl;
+        opShortName = COMMAND_LINE_ARGUMENTS[i].shortName;
+        opLongName = COMMAND_LINE_ARGUMENTS[i].longName;
+        opDescription = COMMAND_LINE_ARGUMENTS[i].description;
+        formatOption(opShortName, opLongName, opDescription);
     };
+}
+
+void formatOption (std::string shortName, std::string longName, std::string description) {
+    std::cout << "  ";
+    std::cout << shortName << ", " << longName << " \t " << description << std::endl;
 }
 
