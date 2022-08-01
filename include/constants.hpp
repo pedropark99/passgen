dd7324e16ef0127ddcb5d715cbcfe32f1bd705c5
pedropark99 <pedropark99@gmail.com>
2022-07-31 22:45:48 -0300

    We can now generate simple and random passwords

---
+++ include/constants.hpp
@@ -0,0 +1,18 @@
+
+char ANSI_CHARS[] = {
+    // LOWER CASE LETTERS
+    'a', 'b', 'c', 'd', 'e', 'f',
+    'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r',
+    's', 't', 'u', 'v', 'x', 'w',
+    'y', 'z',
+    // UPPER CASE LETTERS
+    'A', 'B', 'C', 'D', 'E', 'F',
+    'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'X', 'W',
+    'Y', 'Z',
+    // NUMBERS
+    '0', '1', '2', '3', '4', '5',
+    '6', '7', '8', '9'
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
-#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
-#include "command-line-options.hpp"
+#include "constants.hpp"
+
+int getRandomNumber(int maxLimit) {
+    int number = std::rand() % maxLimit;
+    return number;
+}
+
+void startRandomSeed () {
+    std::srand(time(NULL));
+}
+
 
 
 int main () {
-    printHelp();
+
+    int kpasswordLength = 5;
+    int knumberOfAnsiChars = sizeof(ANSI_CHARS) / sizeof(ANSI_CHARS[0]);
+    
+    int randomIndex;
+    char password[kpasswordLength];
+    startRandomSeed();
+    for (int i = 0; i < kpasswordLength; i++) {
+        randomIndex = getRandomNumber(knumberOfAnsiChars);
+        password[i] = ANSI_CHARS[randomIndex];
+    }
+
+    std::cout << password << std::endl;
+
     return 1;
 }
