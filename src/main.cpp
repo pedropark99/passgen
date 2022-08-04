9914ddd648fa1ce6de5a8e56b5d3ef4804c0f7b8
pedropark99 <pedropark99@gmail.com>
2022-08-03 22:08:03 -0300

    CMD arguments parser working again

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,75 @@
 #include <iostream>
 #include <string.h>
+#include <sstream>
 #include <stdlib.h>
 #include <time.h>
 
-#include <getopt.h>
-#include <unistd.h>
-
 #include "constants.hpp"
 #include "command-line-options.hpp"
+
+// Global variables
+int lflag = 10;
+int sflag = 0;
+char CMD_LINE_OPTIONS[] = {
+    'l', 's'
+};
+
+
+bool isLengthArgument (std::string arg) {
+    return arg == "-l" | arg == "--length";
+}
+
+int castLengthValue (std::string lengthValue) {
+    std::istringstream ss(lengthValue);
+    int castedValue;
+    if (!(ss >> castedValue)) {
+        std::cerr << "Invalid number: " << lengthValue << '\n';
+    } else if (!ss.eof()) {
+        std::cerr << "Trailing characters after number: " << lengthValue << '\n';
+    }
+    return castedValue;
+}
+
+bool isSpecialCharsArgument (std::string arg) {
+    return arg == "-s" | arg == "--special-chars";
+}
+
+
+void checkCmdLineArguments (int argc, char *argv[]) {
+    std::string currentArgument;
+    std::string nextArgument;
+    for (int argumentIndex = 0; argumentIndex < argc; argumentIndex++) {
+        currentArgument = std::string(argv[argumentIndex]);
+        if (isLengthArgument(currentArgument)) {
+            argumentIndex++;
+            nextArgument = std::string(argv[argumentIndex]);
+            lflag = castLengthValue(nextArgument);
+            continue;
+        }
+        if (isSpecialCharsArgument(currentArgument)) {
+            sflag = 1;
+            continue;
+        }
+    }
+}
+
+
+
+
+
+
+int main (int argc, char *argv[]) {
+    checkCmdLineArguments(argc, argv);
+    std::cout << lflag << std::endl;
+    std::cout << sflag << std::endl;
+    return 1;
+}
+
+
+
+
+
+
 
 
 int getRandomNumber (int maxLimit) {
@@ -18,10 +80,6 @@ int getRandomNumber (int maxLimit) {
 void startRandomSeed () {
     std::srand(time(NULL));
 }
-
-char CMD_LINE_OPTIONS[] = {
-    'l', 'b'
-};
 
 std::string generatePassword (int passwordLength) {
     int randomIndex;
@@ -34,44 +92,4 @@ std::string generatePassword (int passwordLength) {
     }
 
     return std::string(password);
-}
-
-
-void checkCmdLineOptions (int argc, char *argv[]) {
-    char *lflag;
-    int bflag = 0;
-    int option;
-    while ((option = getopt(argc, argv, "l:b")) != 1) {
-        switch (option) {
-        case 'l':
-            lflag = optarg;
-            break;
-        case 'b':
-            bflag = 1;
-            break;
-        default:
-            raiseCmdOptionException();
-            abort();
-            break;
-        }
-    }
-
-    std::cout << lflag << std::endl;
-}
-
-
-void raiseCmdOptionException () {
-    std::cout << "Invalid command line option! Available options are: ";
-    for (int i = 0; i < sizeof(CMD_LINE_OPTIONS) / sizeof(CMD_LINE_OPTIONS[0]); i++) {
-        std::cout << CMD_LINE_OPTIONS[i];
-    };
-    std::cout << std::endl;
-}
-
-
-
-int main (int argc, char **argv) {
-    checkCmdLineOptions(argc, argv);
-
-    return 1;
 }
