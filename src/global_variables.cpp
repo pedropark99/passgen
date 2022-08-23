6e60f1552d7f87fedfb48d1362377b9bb8ed2660
pedro.duarte <pedro.duarte@take.net>
2022-08-23 12:13:15 -0300

    Change to UTF-8 characters

--- src/global_variables.cpp
+++ src/global_variables.cpp
@@ -39,32 +39,33 @@ const std::vector<CommandLineOption> COMMAND_LINE_OPTIONS = {
 
 
 const std::vector<char> LOWER_CASE_LETTERS = {
-    'a', 'b', 'c', 'd', 'e', 'f',
-    'g', 'h', 'i', 'j', 'k', 'l',
-    'm', 'n', 'o', 'p', 'q', 'r',
-    's', 't', 'u', 'v', 'x', 'w',
-    'y', 'z'
+    u8'a', u8'b', u8'c', u8'd', u8'e', u8'f',
+    u8'g', u8'h', u8'i', u8'j', u8'k', u8'l',
+    u8'm', u8'n', u8'o', u8'p', u8'q', u8'r',
+    u8's', u8't', u8'u', u8'v', u8'x', u8'w',
+    u8'y', u8'z'
 };
 
 const std::vector<char> UPPER_CASE_LETTERS = {
-    'A', 'B', 'C', 'D', 'E', 'F',
-    'G', 'H', 'I', 'J', 'K', 'L',
-    'M', 'N', 'O', 'P', 'Q', 'R',
-    'S', 'T', 'U', 'V', 'X', 'W',
-    'Y', 'Z',
+    u8'A', u8'B', u8'C', u8'D', u8'E', u8'F',
+    u8'G', u8'H', u8'I', u8'J', u8'K', u8'L',
+    u8'M', u8'N', u8'O', u8'P', u8'Q', u8'R',
+    u8'S', u8'T', u8'U', u8'V', u8'X', u8'W',
+    u8'Y', u8'Z'
 };
 
 const std::vector<char> NUMBERS = {
-    '0', '1', '2', '3', '4', '5',
-    '6', '7', '8', '9'
+    u8'0', u8'1', u8'2', u8'3', u8'4',
+    u8'5', u8'6', u8'7', u8'8', u8'9'
 };
 
 
 const std::vector<char> SPECIAL_CHARS = {
-    '!', '@', '#', '$', '%',
-    '^', '&', '*', '(', ')',
-    '-', '_', '=', '|', '\\',
-    '/', '<', '>', ';', ':',
-    '[', ']', '{', '}', ',',
-    '?', '.', '+'
+    u8'!', u8'@', u8'#', u8'$', u8'%',
+    u8'^', u8'&', u8'*', u8'(', u8')',
+    u8'-', u8'_', u8'=', u8'|', u8'\\',
+    u8'/', u8'<', u8'>', u8';', u8':',
+    u8'[', u8']', u8'{', u8'}', u8',',
+    u8'?', u8'.', u8'+', u8'~', u8'`',
+    u8'\'', u8'\u0022'
 };
