465b1eada64943b7d28e4a98719bc124a4f8321a
pedropark99@gmail.com <pedro.duarte@take.net>
2022-08-24 20:28:42 -0300

    Knit engine is working now!

--- docs/cmd_knit_engine.R
+++ docs/cmd_knit_engine.R
@@ -1,4 +1,5 @@
 library(knitr)
+library(stringr)
 library(fs)
 
 get_build_directory <- function(){
@@ -6,11 +7,24 @@ get_build_directory <- function(){
   return(build_dir)
 }
 
+get_cmd_output <- function(text){
+  text <- text[-1:-5]
+  fixed_text <- str_replace(
+    text, 
+    "^[C/]:?([-_.A-Za-z0-9/ ]|\\\\)+build>",
+    "terminal$ "
+  )
+  n <- length(fixed_text)
+  fixed_text <- fixed_text[-c(n, n - 1)]
+  return(fixed_text)
+}
+
 cmd_knit_engine <- function(options){
   code <- options$code
   build_dir <- get_build_directory()
   change_dir_cmd <- paste("cd", build_dir)
   out <- system2("cmd", input = c(change_dir_cmd, code), stdout = TRUE)
+  out <- get_cmd_output(out)
   knitr::engine_output(options, code, out)
 }
 
